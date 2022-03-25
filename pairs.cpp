55cefd61d2b46e6bfd10c6e15d627fb6ccb87f2d
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-03-25 12:01:13 +0600

    Pairs O(nLog N) added

--- pairs.cpp
+++ pairs.cpp
@@ -1,7 +1,7 @@
 // Pairs
 // Given an array and a number n
 // You have to find out the pairs that form the sum of number n
-// Time Complexity: O(n^2)
+// Time Complexity: O(nLog N)
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -11,14 +11,17 @@ int main(){
 	vector<int> v = {10, 5, 2, 3, -6, 9, 11};
 	int num = 21;
 
-	for (int i = 0; i < v.size(); ++i)
-	{
-		for (int j = i + 1; j < v.size(); ++j)
-		{
-			if(v[i] + v[j] == num){
-				cout << v[i] << ", " << v[j] << endl;
-			}
+	int start = 0;
+	int end = v.size() - 1;
+
+	while(start != end){
+
+		if(v[start] + v[end] == num){
+			cout << v[start] << ", " << v[end] << endl;
 		}
+
+		start ++;
+		end --;
 	}
 
 
