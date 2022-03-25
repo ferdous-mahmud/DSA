be1b298ecc5e1f6ea879f0a8d3b85fec3648979b
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-03-25 19:23:15 +0600

    triplets O(n ^ 2) whithout dsa added

--- triplets.cpp
+++ triplets.cpp
@@ -1,7 +1,8 @@
 // Triplets
 // Given an array and a number n
 // You have to find 3 element of array that can form the sum equals to n
-// Time Complexity: O(n ^ 2)
+// Time Complexity: O(n ^ 2) 
+// Less memory use(Without any data structure)
 
 #include<iostream>
 #include<vector>
@@ -12,23 +13,28 @@ int main(){
 	vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 15};
 	int sum = 18;
 
-	for (int i = 0; i < v.size(); ++i)
+	for (int i = 0; i < v.size() - 3; ++i)
 	{
+		int first = i + 1;
+		int last = v.size() - 1;
 		int rem = sum - v[i];
-		unordered_set<int> s;
 
-		for (int j = i + 1; j < v.size(); ++j)
-		{
-			int nowRem = rem - v[j];
+		while(first < last){
 
-			if(s.find(nowRem) != s.end()){
-				cout << v[i] << ", " << nowRem << ", " << v[j]  << endl;
+			int s = v[first] + v[last];
+
+			if(s == rem){
+				cout << v[i] << ", " << v[first] << ", " << v[last] << endl;
+				first ++;
+				last --;
+			}else if(s > rem){
+				last --;
+			}else{
+				first ++;
 			}
 
-			s.insert(v[j]);
 		}
 	}
-
 
 	return 0;
 }
